8cfda5e9992a1ea614227c813ecd36e597559471
aastha4 <aasthapoudel912@gmail.com>
2024-06-27 16:27:41 +0545

    [add]

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connecion With MongoDB
-mongoose.connect("mongodb+srv://aastha:aasthap0210@cluster0.7mdbjip.mongodb.net/DIVA")
+mongoose.connect("mongodb+srv://aastha:aasthappassword@cluster0.oextlx6.mongodb.net/DIVA")
+
+
 
 //API Creation
 
@@ -24,9 +26,84 @@ app.get("/",(req,res)=>{
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename:(req,file,cb)=>{
-        return cb(null,'${file.fieldname}_${Date.now()}${path.extname(file.originalname)}')
-    }
-}) 
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+}
+})  
+
+const upload = multer({storage:storage})
+
+
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+    
+})
+
+//schema for creating products
+
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required:true,
+    },
+
+    name:{
+            type:String,
+            required:true,
+    },
+
+    image:{
+            type:String,
+            required:true,
+    }, 
+    
+    category:{
+        type:String,
+        required:true,
+    },
+    
+    new_price:{
+        type:Number,
+        required:true,
+    },
+
+    old_price:{
+        type:Number,
+        required:true,
+    },
+
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
 
 app.listen(port,(error)=>{
     if (!error) {
@@ -34,7 +111,7 @@ app.listen(port,(error)=>{
     }
     else
     {
-        console.log("Error HEllo: "+error)
+        console.log("Error : "+error)
     }
 })
 
