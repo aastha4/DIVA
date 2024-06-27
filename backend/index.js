aadf1ac926c9c6a99cd471a29496abca75f8a157
aastha4 <aasthapoudel912@gmail.com>
2024-06-27 13:47:36 +0545

    frontend Complete

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connecion With MongoDB
+mongoose.connect("mongodb+srv://aastha:aasthap0210@cluster0.7mdbjip.mongodb.net/DIVA")
+
+//API Creation
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+app.listen(port,(error)=>{
+    if (!error) {
+        console.log("Server Running on Port "+port) 
+    }
+    else
+    {
+        console.log("Error : "+error)
+    }
+})
+
+
+
+
